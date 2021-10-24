ea918d20616603caf27eee9b501aa0bca5de53ed
fkoll <fkoll01@gmail.com>
2021-10-24 16:54:11 +0200

    initializer bug fix

--- js/standard.js
+++ js/standard.js
@@ -9,11 +9,18 @@ function invertColor(hexTripletColor) {
     return color;
 }
 
+window.addEventListener("load", initialize, false);
 
 
-var styles = getComputedStyle(document.documentElement);
-var colorValue = styles.getPropertyValue('--primary-color');
-
+function initialize() {
+    if(localStorage[initialization] != true){
+    localStorage['primary-color'] = '#0000FF';
+    localStorage['secondary-color'] = '#0000FF';
+    localStorage['background-color'] = '#000000';
+    localStorage['invertedbackground'] = '#FFFFFF';
+    localStorgae['initialization'] = 'true'; 
+    }
+}
 
 document.documentElement.style.setProperty('--primary-color', localStorage['primary-color']);
 document.documentElement.style.setProperty('--secondary-color', localStorage['secondary-color']);
